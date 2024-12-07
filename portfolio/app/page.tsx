10dd1d6f052ee88a457594bc9d96bd89d6f05f9e
maraiii <mariaeduantunes200281@gmail.com>
2024-12-07 18:53:13 -0300

    Remoção de importações não utilizadas

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,11 +2,8 @@ import About from "./About";
 import Footer from "./footer";
 import Header from "./header";
 import HeroSection from "./herosection";
-import LatestProjects from "./LatestProjects";
-import MoreAboutMe from "./MoreAboutMe";
 import Projects from "./Projects";
 import Skills from "./Skills";
-import SkillsAndAchievements from "./SkillsAndAchievements";
 
 export default function Home() {
   return (
